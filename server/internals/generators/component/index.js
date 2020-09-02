98b82b0226283e031e45cd6b761b175693e4c086
HamidReazaNikoonia <hamidrezanik00ni@gmail.com>
2020-09-02 05:38:02 +0430

    dsd

---
+++ server/internals/generators/component/index.js
@@ -0,0 +1,57 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Component Generator
+ */
+
+/* eslint strict: ["off"] */
+
+'use strict';
+
+const componentExists = require('../utils/componentExists');
+
+module.exports = {
+  description: 'Add an unconnected component',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What should it be called?',
+      default: 'Button',
+      validate: value => {
+        if (/.+/.test(value)) {
+          return componentExists(value)
+            ? 'A component or container with this name already exists'
+            : true;
+        }
+
+        return 'The name is required';
+      },
+    },
+  ],
+  // eslint-disable-next-line no-unused-vars
+  actions: data => {
+    // Generate index.js and index.test.js
+    const actions = [
+      {
+        type: 'add',
+        path: '../../../src/components/{{properCase name}}/index.js',
+        templateFile: './component/index.js.hbs',
+        abortOnFail: true,
+      },
+      {
+        type: 'add',
+        path: '../../../src/components/{{properCase name}}/tests/index.test.js',
+        templateFile: './component/index.test.js.hbs',
+        abortOnFail: true,
+      },
+      {
+        type: 'add',
+        path: '../../../src/components/{{properCase name}}/tests/__snapshots__/index.test.js.snap',
+        templateFile: './component/index.test.js.snap.hbs',
+        abortOnFail: true,
+      },
+    ];
+
+    return actions;
+  },
+};
